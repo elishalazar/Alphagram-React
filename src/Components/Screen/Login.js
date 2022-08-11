e2f635a4a7aea4fd4f9f0cfe17b6ea924a2eb94c
Elisha-Lazar <elishalazar@flaconit.com.pk>
2022-08-11 16:13:00 +0500

    changes

--- src/Components/Screen/Login.js
+++ src/Components/Screen/Login.js
@@ -1,69 +1,116 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    // const history = useHistory()
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const PostData = () => {
-        ('POST', "/signup", JSON.stringify({
-            name, email, password
 
-        })).then(res => res.json())
+
+
+
+
+
+    const Signin = (e) => {
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            toast.error("Invalid Email")
+            return
+        }
+        e.preventDefault()
+
+
+        fetch("/signup", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        }).then(res => res.json())
             .then(data => {
+                console.log(data, "fffefrew");
                 if (data.error) {
-                    console.log(data);
-
+                    toast.error(data.error)
                 }
                 else {
-                    console.log(data);
-                    setName('')
-
+                    toast.success(data.message);
+                    // history.push('/login')
                 }
+
+
+            }).catch(err => {
+                console.log(err);
             })
+
     }
 
+
+
+    // ------------------Login function-------------------
+
+    const Login = (e) => {
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            toast.error("Invalid Email")
+            return
+        }
+        e.preventDefault()
+
+
+        fetch("/signin", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+
+                email,
+                password
+            })
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data, "fffefrew");
+                if (data.error) {
+                    toast.error(data.error)
+                }
+                else {
+                    toast.success(data.message);
+                    // history.push('/login')
+                }
+
+
+            }).catch(err => {
+                console.log(err);
+            })
+
+    }
+
+
+
+    const showToast = () => {
+
+    };
     const [rightClass, setRightClass] = React.useState(true);
+
+
+
+
+
+
+
+
+
     return (
         <div className="App" >
+            <ToastContainer />
             <div class={`${rightClass && "right-panel-active"} container`}
                 id="containers">
-
-                <div class="form-container sign-in-container">
-                    <form action="#">
-                        <h1>Sign in</h1>
-
-                        <span>or use your account</span>
-
-                        <div className="field">
-                            <Box
-                                component="form"
-                                sx={{
-                                    "& > :not(style)": { m: 1, width: "25ch" },
-                                }}
-                                noValidate
-                                autoComplete="off"
-                            >
-                                <TextField
-                                    id="outlined-basic"
-                                    label="Email"
-                                    variant="outlined"
-                                    type="email"
-                                />
-                                <TextField
-                                    id="outlined-basic"
-                                    label="Email"
-                                    variant="outlined"
-                                    type="password"
-                                />
-
-                            </Box>
-                        </div>
-                        <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
-                    </form>
-                </div>
                 <div class="form-container sign-up-container">
                     <form action="#">
                         <h1>Create Account</h1>
@@ -107,35 +154,91 @@ const Login = () => {
                             </Box>
                         </div>
                         <button
-                            onClick={() => {
-                                PostData();
+                            onClick={(e) => {
+                                Signin(e);
+                                showToast();
 
                             }}
                         >Sign Up</button>
+
                     </form>
                 </div>
 
 
 
 
+                <div class="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Sign in</h1>
 
+                        <span>or use your account</span>
 
+                        <div className="field">
+                            <Box
+                                component="form"
+                                sx={{
+                                    "& > :not(style)": { m: 1, width: "25ch" },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <TextField
+                                    id="outlined-basic"
+                                    label="Email"
+                                    variant="outlined"
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <TextField
+                                    id="outlined-basic"
+                                    label="password"
+                                    variant="outlined"
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
 
+                            </Box>
+                        </div>
+                        <a href="#">Forgot your password?</a>
+                        <button
 
+                            onClick={(e) => {
+                                Login(e);
+                                showToast();
 
+                            }}
 
-
-
-
-
+                        >Sign In</button>
+                    </form>
+                </div>
                 <div class="overlay-container">
                     <div class="overlay">
-
+                        <div class="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>
+                                To keep connected with us please login with your personal info
+                            </p>
+                            <button class="ghost" id="signIn"
+                                onClick={() => {
+                                    setEmail('')
+                                    setName("")
+                                    setPassword("")
+                                    setRightClass(false);
+                                }}
+                            >
+                                Sign In
+                            </button>
+                        </div>
                         <div class="overlay-panel overlay-right">
                             <h1>Hello, Friend!</h1>
                             <p>Enter your personal details and start journey with us</p>
                             <button class="ghost" id="signUp"
                                 onClick={() => {
+
+                                    setEmail('')
+                                    setPassword('')
 
                                     setRightClass(true)
                                 }}
@@ -143,18 +246,6 @@ const Login = () => {
                                 Sign Up
                             </button>
                         </div>
-                        <div class="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>
-                                To keep connected with us please login with your personal info
-                            </p>
-                            <button class="ghost" id="signIn"
-                                onClick={() => setRightClass(false)}
-                            >
-                                Sign In
-                            </button>
-                        </div>
-
                     </div>
                 </div>
             </div >
--- src/Components/Screen/Sigin_Page.js
+++ src/Components/Screen/Sigin_Page.js
@@ -1,32 +1,115 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import { Link, useHistory } from "react-router-dom"
 const Sigin_Page = () => {
+
+    // const history = useHistory()
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const PostData = () => {
-        ('POST', "/signup", JSON.stringify({
-            name, email, password
 
-        })).then(res => res.json())
+
+
+
+
+
+    const Signin = (e) => {
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            toast.error("Invalid Email")
+            return
+        }
+        e.preventDefault()
+
+
+        fetch("/signup", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        }).then(res => res.json())
             .then(data => {
+                console.log(data, "fffefrew");
                 if (data.error) {
-                    console.log(data);
-
+                    toast.error(data.error)
                 }
                 else {
-                    console.log(data);
-                    setName('')
-
+                    toast.success(data.message);
+                    // history.push('/login')
                 }
+
+
+            }).catch(err => {
+                console.log(err);
             })
+
     }
 
+
+
+    // ------------------Login function-------------------
+
+    const Login = (e) => {
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            toast.error("Invalid Email")
+            return
+        }
+        e.preventDefault()
+
+
+        fetch("/signin", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+
+                email,
+                password
+            })
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data, "fffefrew");
+                if (data.error) {
+                    toast.error(data.error)
+                }
+                else {
+                    toast.success(data.message);
+                    // history.push('/login')
+                }
+
+
+            }).catch(err => {
+                console.log(err);
+            })
+
+    }
+
+
+
+    const showToast = () => {
+
+    };
     const [rightClass, setRightClass] = React.useState(true);
+
+
+
+
+
+
+
+
+
     return (
         <div className="App" >
+            <ToastContainer />
             <div class={`${rightClass && "right-panel-active"} container`}
                 id="containers">
                 <div class="form-container sign-up-container">
@@ -72,13 +155,19 @@ const Sigin_Page = () => {
                             </Box>
                         </div>
                         <button
-                            onClick={() => {
-                                PostData();
+                            onClick={(e) => {
+                                Signin(e);
+                                showToast();
 
                             }}
                         >Sign Up</button>
+
                     </form>
                 </div>
+
+
+
+
                 <div class="form-container sign-in-container">
                     <form action="#">
                         <h1>Sign in</h1>
@@ -99,18 +188,30 @@ const Sigin_Page = () => {
                                     label="Email"
                                     variant="outlined"
                                     type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                                 <TextField
                                     id="outlined-basic"
-                                    label="Email"
+                                    label="password"
                                     variant="outlined"
                                     type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                 />
 
                             </Box>
                         </div>
                         <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
+                        <button
+
+                            onClick={(e) => {
+                                Login(e);
+                                showToast();
+
+                            }}
+
+                        >Sign In</button>
                     </form>
                 </div>
                 <div class="overlay-container">
@@ -121,7 +222,12 @@ const Sigin_Page = () => {
                                 To keep connected with us please login with your personal info
                             </p>
                             <button class="ghost" id="signIn"
-                                onClick={() => setRightClass(false)}
+                                onClick={() => {
+                                    setEmail('')
+                                    setName("")
+                                    setPassword("")
+                                    setRightClass(false);
+                                }}
                             >
                                 Sign In
                             </button>
@@ -131,6 +237,9 @@ const Sigin_Page = () => {
                             <p>Enter your personal details and start journey with us</p>
                             <button class="ghost" id="signUp"
                                 onClick={() => {
+
+                                    setEmail('')
+                                    setPassword('')
 
                                     setRightClass(true)
                                 }}
